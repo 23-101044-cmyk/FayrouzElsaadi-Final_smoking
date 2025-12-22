30bcd3363770254ee8263da774ed4a1a452b6727
Fayrouz El-Saadi <23-101044@students.eui.edu.eg>
2025-12-22 23:59:29 +0200

    about

--- home.js
+++ home.js
@@ -5,7 +5,7 @@ const nextBtn = document.querySelector('.arrow2');
 
 let currentIndex = 0;
 const totalSlides = slides.length;
-const intervalTime = 2500;
+const intervalTime = 3500;
 let autoSlide;
 
 
@@ -51,3 +51,44 @@ prevBtn.addEventListener('click', () => {
 
 /* Init */
 startAutoSlide();
+
+
+
+let array1 = [
+    {
+        img: "assets/footerimg1.png",
+        title: "Healthier Lungs",
+        text: "Within weeks of quitting, lung function improves and breathing becomes easier.",
+    },
+        {
+        img: "assets/footerimg2.png",
+        title: "Financial Freedom",
+        text: "Save thousands yearly and invest in what truly matters to you.",
+    },
+        {
+        img: "assets/footerimg3.png",
+        title: "Better Quality of Life",
+        text: "More energy, better sleep, and improved overall well-being.",
+    },
+
+];
+
+
+let html = "";
+for (let i = 0; i < array1.length; i++) {
+  html += `
+
+
+                <div class="card2">
+                    <div class="card22imgdiv">
+                        <img src="${array1[i].img}" alt="" class="card22img">
+                        <h5 class="card22title">${array1[i].title}</h5>
+                    </div>
+                    <p class="card22p">${array1[i].text}</p>
+                </div>
+        
+    
+  `;
+}
+
+document.getElementById("array1").innerHTML = html;
